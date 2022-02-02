185873d79803679eadf454a40f7a39bebff4e797
jpdrsanchez <joaopedrosanchez302@gmail.com>
2022-02-02 08:36:31 -0300

    chore: improve jest configuration

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,16 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
+  collectCoverageFrom: [
+    'src/**/*.ts(x)',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.tsx'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }
 
 module.exports = createJestConfig(customJestConfig)
